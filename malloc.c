a0272c6effd3c47826251ad44a8dfb362bb90f7a
Will Brown <kyrosewoodgang@gmail.com>
2022-10-26 17:01:25 -0400

    Add more safety, more standard-compliant behavior.

--- malloc.c
+++ malloc.c
@@ -12,6 +12,7 @@
 #include <stdalign.h>
 #include <stdint.h>
 #include <string.h>
+#include <errno.h>
 
 #include <unistd.h>
 #include <sys/mman.h>
@@ -49,16 +50,19 @@ static struct block_hdr *create_region(size_t size) {
 		NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0
 	);
 	
-	if (new_region == MAP_FAILED) return NULL;
-	struct block_hdr hdr = {
-		.region = new_region,
-		.next = NULL,
-		.prev = NULL,
-		.size = size - sizeof(struct block_hdr)
-	};
-	
-	*new_region = hdr;
-	return new_region;
+	if (new_region == MAP_FAILED) {
+		errno = ENOMEM;
+		return NULL;
+	} else {
+		*new_region = (struct block_hdr) {
+			.region = new_region,
+			.next = NULL,
+			.prev = NULL,
+			.size = size - sizeof(struct block_hdr)
+		};
+		
+		return new_region;
+	}
 }
 
 static struct block_hdr *alloc_block(struct block_hdr *block, size_t size) {
@@ -93,6 +97,7 @@ static struct block_hdr *find_block(size_t size) {
 		long pagesize = sysconf(_SC_PAGESIZE);
 		size_t region_size = ALIGN(size + sizeof(struct block_hdr), pagesize);
 		struct block_hdr *new_region = create_region(region_size);
+		if (!new_region) return NULL;
 		new_region->prev = block;
 		block->next = new_region;
 		block = new_region;
@@ -104,15 +109,16 @@ static struct block_hdr *find_block(size_t size) {
 void *malloc(size_t size) {
 	size = ALIGN(size, ALIGNMENT);
 	struct block_hdr *block = find_block(size);
-	if (block) return alloc_block(block, size) + 1;
-	else return NULL;
+	return block ? alloc_block(block, size) + 1 : NULL;
 }
 
 void free(void *ptr) {
+	if (!ptr) return;
+	
 	struct block_hdr *block = (struct block_hdr *) ptr - 1;
 	
 	// Merge adjacent blocks
-	size_t new_block_size = block->size & ~1UL;
+	size_t new_block_size = block->size & SIZE_MAX - 1;
 	struct block_hdr *new_block = block;
 	
 	// Previous block is in the same region and free
@@ -148,16 +154,19 @@ void free(void *ptr) {
 }
 
 void *calloc(size_t nmemb, size_t size) {
-	// overflow check
-	if (nmemb > SIZE_MAX / size) return NULL;
-	
-	size_t n = nmemb * size;
-	void *ptr = malloc(n);
-	if (!ptr) return NULL;
-	else return memset(ptr, 0, n);
+	size_t n;
+	if (!__builtin_mul_overflow(nmemb, size, &n)) {
+		errno = ENOMEM;
+		return NULL;
+	} else {
+		void *ptr = malloc(n);
+		return ptr ? memset(ptr, 0, n) : NULL;
+	}
 }
 
 void *realloc(void *ptr, size_t size) {
+	if (!ptr) return malloc(size);
+
 	size = ALIGN(size, ALIGNMENT);
 	struct block_hdr *block = (struct block_hdr *) ptr - 1;
 	
@@ -167,19 +176,19 @@ void *realloc(void *ptr, size_t size) {
 		block->next &&
 		block->next->region == block->region &&
 		!(block->next->size & 1) &&
-		(combined_block_size = block->next->size + (block->size & ~1UL) + sizeof(struct block_hdr)) >= size
+		(combined_block_size = block->next->size + (block->size & SIZE_MAX - 1) + sizeof(struct block_hdr)) >= size
 	) {
 		size_t leftover_block_size = combined_block_size - size;
 		if (leftover_block_size > sizeof(struct block_hdr)) {
-			struct block_hdr *new_block = (struct block_hdr *)
+			struct block_hdr *free_block = (struct block_hdr *)
 				((char *) block + sizeof(struct block_hdr *) + size);
 
-			new_block->region = block->region;
-			new_block->next = block->next->next;
-			if (new_block->next) new_block->next->prev = new_block;
-			new_block->prev = block;
-			new_block->size = leftover_block_size - sizeof(struct block_hdr);
-			block->next = new_block;
+			free_block->next = block->next->next; // This is first in case the headers overlap
+			if (free_block->next) free_block->next->prev = free_block;
+			free_block->region = block->region;
+			free_block->prev = block;
+			free_block->size = leftover_block_size - sizeof(struct block_hdr);
+			block->next = free_block;
 			block->size = size;
 		} else {
 			block->next = block->next->next;
